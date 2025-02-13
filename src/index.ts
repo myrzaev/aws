5824268bcf2e4e89b09b73187666537a2d9690f2
Timur Myrzaev <tmyrzaev99@gmail.com>
2025-02-12 23:29:18 -0500

    change response

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, Shrey!");
+  res.send("Hello World!");
 });
 
 app.get("/api", (req: Request, res: Response) => {
